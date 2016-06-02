92e038ec41043f9f90e43ab4dc94d85d2c4f07fc
Emmanuel Cron <ecron@google.com>
2016-06-01 17:04:47 -0700

    Fixed requirements versions. Ignored Eclipse's project files. Moved app_info.py.ex to app_info.py in twitchalerts so we can get a home page

--- alerts/twitchalerts/app_info.py.ex
+++
@@ -1,6 +0,0 @@
-# Configure at http://www.twitchalerts.com/oauth/apps/register
-TWITCH_INFO = {
-    'client_id': '',
-    'client_secret': '',
-    'redirect_uri': 'http://localhost:8000/twitchalerts/authorize'
-}
