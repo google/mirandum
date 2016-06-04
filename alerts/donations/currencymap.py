3112ff56e43d91d7e1bcff747dff5d434316897b
Christopher Schmidt <crschmidt@crschmidt.net>
2016-06-04 10:43:50 -0400

    Use unicode for currency symbols.

--- alerts/donations/currencymap.py
+++ alerts/donations/currencymap.py
@@ -13,8 +13,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License. 
 SYMBOLS = {
-    'USD': '$',
-    'AUD': 'A$',
-    'EUR': '€',
-    'CAD': 'C$'
+    'USD': u'$',
+    'AUD': u'A$',
+    'EUR': u'€',
+    'CAD': u'C$'
 }
